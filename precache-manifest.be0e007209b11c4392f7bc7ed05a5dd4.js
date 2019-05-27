372ded1db3ef3fc27354714d0277cad786657e1a
Anastasia <Anastasia@MacBook-Pro-3.local>
2019-05-27 23:05:36 +0300

    Updates

---
+++ precache-manifest.be0e007209b11c4392f7bc7ed05a5dd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ad541981a3ff2e2c12401e783ec2ed",
+    "url": "/insta-react/index.html"
+  },
+  {
+    "revision": "234168c45cfd25c7579c",
+    "url": "/insta-react/static/css/main.1e7af6ce.chunk.css"
+  },
+  {
+    "revision": "cc30ee79e107ebf33c91",
+    "url": "/insta-react/static/js/2.ab2d1335.chunk.js"
+  },
+  {
+    "revision": "234168c45cfd25c7579c",
+    "url": "/insta-react/static/js/main.0e582b38.chunk.js"
+  },
+  {
+    "revision": "29510885df5ca466c9b4",
+    "url": "/insta-react/static/js/runtime~main.668963e6.js"
+  },
+  {
+    "revision": "3b8ad90a7a5366149b0595e667396d65",
+    "url": "/insta-react/static/media/error.3b8ad90a.png"
+  },
+  {
+    "revision": "9e8a2b3937952b3de7aca04cb0d74020",
+    "url": "/insta-react/static/media/logo.9e8a2b39.svg"
+  }
+]);
