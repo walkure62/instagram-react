efeda6004753d1a14fba460aa146672bf380273a
Anastasia <Anastasia@MacBook-Pro-3.local>
2019-05-27 18:46:29 +0300

    add users account

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class User extends Component {
     render() {
         return (
-            <a href="#" className={this.props.min ? "user min" : "user"}>
+            <a href="#" className="user">
                 <img src={this.props.src} alt={this.props.alt}></img>
                 <div>{this.props.name}</div>
             </a>
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,36 +1,66 @@
-import React from 'react';
+import React, {Component} from 'react';
+import InstaService from '../services/instaservice';
 import User from './User';
 
-export default function Users() {
-    return (
-        <div className="right">
-            <User
-                src="https://i.ytimg.com/vi/d4oG8_3j58U/maxresdefault.jpg"
-                alt="girl"
-                name="Walkure"
-                />
-            <div className="users__block">
-              <User
-                src="https://i.ytimg.com/vi/d4oG8_3j58U/maxresdefault.jpg"
-                alt="girl"
-                name="Walkure"
-                min />
-                <User
-                src="https://i.ytimg.com/vi/d4oG8_3j58U/maxresdefault.jpg"
-                alt="girl"
-                name="Walkure"
-                min />
-                <User
-                src="https://i.ytimg.com/vi/d4oG8_3j58U/maxresdefault.jpg"
-                alt="girl"
-                name="Walkure"
-                min />
-                <User
-                src="https://i.ytimg.com/vi/d4oG8_3j58U/maxresdefault.jpg"
-                alt="girl"
-                name="Walkure"
-                min />
+export default class Users extends Component {
+    InstaService = new InstaService();
+
+    state = {
+        users: [],
+        error: false
+    }
+
+    componentDidMount() {
+        this.updateUsers();
+
+    }
+
+    updateUsers() {
+        this.InstaService.getAllPosts()
+        .then(this.onUsersLoaded)
+        .catch(this.onError);
+    }
+
+    onUsersLoaded = (users) => {
+        this.setState({
+            users: users,
+            error: false
+        })
+
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true
+        })
+    }
+
+
+    renderUsers(arr) {
+        return arr.map((user) => {
+            const {photo, name, altName, id} = user;
+
+            return (
+                <a href="#" key={id} className="user min">
+                <img src={photo} alt={altName}></img>
+                <div>{name}</div>
+            </a>
+            )
+        })
+    }
+    render() {
+        const {users} = this.state;
+       
+
+        const items = this.renderUsers(users);
+        return  (
+            <div className="right">
+                <User 
+                src="https://pp.userapi.com/c854328/v854328696/225a2/X2tkGXjGy3c.jpg" 
+                alt="Anastasia" 
+                name="walkure62"/>
+                {items}
             </div>
-        </div>
-    )
+        )
+    }
 }
